74c6135183d8a82cb1e2a6cee216d4266fa73da0
mustaphabousildev <bousilmustapha@gmail.com>
2024-02-03 13:55:19 +0100

    micro connection

---
+++ node_modules/host/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+    <Header/>
+    <h3 class="mt-10 text-3xl font-bold">Host</h3>
+  </div>
+</template>
+
+
+<script>
+  import Header from 'remote/Header'
+
+  export default {
+    components: {
+      Header,
+    }
+  }
+</script>
